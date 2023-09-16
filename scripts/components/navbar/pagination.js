5660e50954f0be4c850ba2493e9012fe3777eca9
Daniel Ceriana <danielcerianax@gmail.com>
2023-09-16 20:46:46 -0300

    cambios en paginacion

--- scripts/components/navbar/pagination.js
+++ scripts/components/navbar/pagination.js
@@ -1,22 +1,28 @@
 const paginationbuttons = document.querySelectorAll(".pagination-button");
 const goLeftButton = document.querySelector(".left-button");
 const goRightButton = document.querySelector(".left-right");
-let currentPage;
 
-function setPagination(numberOfPages) {
+function setPagination(numberOfPages, currentPage = 3) {
     console.log(numberOfPages)
     console.log(paginationbuttons)
 
     paginationbuttons[paginationbuttons.length - 1].textContent = numberOfPages
     paginationbuttons[0].textContent = 1
 
+    //setea como active al boton del medio en paginacion
+    if (currentPage >= 3) {
+        paginationbuttons[2].setAttribute("data-is-active", "active")
+    }
+    console.log(paginationbuttons[2].dataset)
 
     if (numberOfPages < 7) {
-        for (let i = 0; i < paginationbuttons.length; i++) {
-            console.log(paginationbuttons[i])
+        for (let i = 1; i < paginationbuttons.length - 1; i++) {
+            console.log(paginationbuttons[i].style.display = "none")
         }
     } else {
-
+        for (let i = 1; i < paginationbuttons.length - 1; i++) {
+            console.log(paginationbuttons[i].textContent)
+        }
     }
 
 }
